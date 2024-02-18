baba43594458676f962c5af73acd507e6e1f2642
Pavel Chernyshev <bungostraygit@gmail.com>
2024-02-18 06:20:17 +0300

    написана функция разности множеств

--- libs/data_structures/unordered_set/unordered_set.c
+++ libs/data_structures/unordered_set/unordered_set.c
@@ -1,5 +1,6 @@
 #include "unordered_set.h"
 
+
 unordered_array_set unordered_array_set_create(size_t capacity) {
     int *data = malloc(sizeof(int) * capacity);
     if (data == NULL) {
@@ -14,6 +15,7 @@ unordered_array_set unordered_array_set_create(size_t capacity) {
     };
 }
 
+
 void unordered_array_set_shrinkToFit(unordered_array_set *set) {
     if (set->size != set->capacity) {
         set->data = (int *) realloc(set->data, sizeof(int) * set->size);
@@ -26,9 +28,10 @@ void unordered_array_set_shrinkToFit(unordered_array_set *set) {
     }
 }
 
+
 unordered_array_set unordered_array_set_create_from_array(const int *arr, size_t arr_size) {
     unordered_array_set set = unordered_array_set_create(arr_size);
-    for (int i = 0; i < arr_size; i++) {
+    for (size_t i = 0; i < arr_size; i++) {
         unordered_array_set_insert(&set, arr[i]);
     }
     unordered_array_set_shrinkToFit(&set);
@@ -36,17 +39,20 @@ unordered_array_set unordered_array_set_create_from_array(const int *arr, size_t
     return set;
 }
 
+
 size_t unordered_array_set_ind(unordered_array_set set, int value) {
     return getFirstIndexOfNumber_(set.data, set.size, value);
 }
+
 
 bool unordered_array_set_isValueIn(unordered_array_set set, int value) {
     return unordered_array_set_ind(set, value) != set.size;
 }
 
+
 bool unordered_array_set_isSubset(unordered_array_set subset, unordered_array_set set) {
     size_t matches = 0;
-    for (int i = 0; i < subset.size; i++) {
+    for (size_t i = 0; i < subset.size; i++) {
         if (unordered_array_set_isValueIn(set, subset.data[i]))
             matches++;
     }
@@ -54,9 +60,11 @@ bool unordered_array_set_isSubset(unordered_array_set subset, unordered_array_se
     return matches == subset.size;
 }
 
+
 void unordered_array_set_isAbleAppend(unordered_array_set *set) {
     assert(set->size < set->capacity);
 }
+
 
 void unordered_array_set_insert(unordered_array_set *set, int value) {
     unordered_array_set_isAbleAppend(set);
@@ -66,24 +74,23 @@ void unordered_array_set_insert(unordered_array_set *set, int value) {
     }
 }
 
+
 void unordered_array_set_deleteElement(unordered_array_set *set, int value) {
     size_t index_of_value = unordered_array_set_ind(*set, value);
     if (index_of_value < set->size)
         deleteByIndexAndSaveOrder_(set->data, &set->size, index_of_value);
 }
 
+
 unordered_array_set unordered_array_set_union(unordered_array_set set1,
                                               unordered_array_set set2) {
-     unordered_array_set result = set1;
-     result.capacity = set1.size + set2.size;
-     result.data = (int *)realloc(result.data, sizeof(int) * result.capacity);
-     if (result.data == NULL) {
-         fprintf(stderr, "Fail memory allocated in unordered_array_set_union.\n"
-                         "File: libs/data_structures/unordered_set/unordered_set.h");
-         exit(-1);
+     unordered_array_set result = unordered_array_set_create(set1.size + set2.size);
+
+     for (size_t i = 0; i < set1.size; i++) {
+         result.data[i] = set1.data[i];
      }
 
-     for (int i = 0; i < set2.size; i++) {
+     for (size_t i = 0; i < set2.size; i++) {
          unordered_array_set_insert(&result, set2.data[i]);
      }
 
@@ -92,10 +99,11 @@ unordered_array_set unordered_array_set_union(unordered_array_set set1,
      return result;
 }
 
+
 unordered_array_set unordered_array_set_intersection(unordered_array_set set1,
                                                      unordered_array_set set2) {
     unordered_array_set result = unordered_array_set_create(set1.size);
-    for (int i = 0; i < set1.size; i++) {
+    for (size_t i = 0; i < set1.size; i++) {
         if (unordered_array_set_isValueIn(set2, set1.data[i]))
             unordered_array_set_insert(&result, set1.data[i]);
     }
@@ -105,13 +113,19 @@ unordered_array_set unordered_array_set_intersection(unordered_array_set set1,
     return result;
 }
 
+
 unordered_array_set unordered_array_set_difference(unordered_array_set set1,
                                                    unordered_array_set set2) {
-    unordered_array_set result = set1;
-    for (int i = 0; i < set1.size; i++) {
-        if (int)
+    unordered_array_set result = unordered_array_set_create(set1.size);
+
+    for (size_t i = 0; i < set1.size; i++) {
+        if (!unordered_array_set_isValueIn(set2, set1.data[i]))
+            result.data[result.size++] = set1.data[i];
     }
 
+    unordered_array_set_shrinkToFit(&result);
+
+    return result;
 }
 
 
--- main.c
+++ main.c
@@ -1,5 +1,13 @@
 #include "libs/data_structures/unordered_set/unordered_set.h"
 
 int main() {
+    int arr1[] = {1, 3, 5, 7};
+    int arr2[] = {2, 4, 6, 8};
 
+    unordered_array_set set1 = unordered_array_set_create_from_array(arr1, 4);
+    unordered_array_set set2 = unordered_array_set_create_from_array(arr2, 4);
+
+    unordered_array_set res = unordered_array_set_union(set1, set2);
+
+    outputArray_(res.data, res.size);
 }
