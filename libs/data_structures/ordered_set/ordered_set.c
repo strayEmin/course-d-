a6daad904e4463fad260430c76ebfb0cc96c788f
Pavel Chernyshev <bungostraygit@gmail.com>
2024-02-29 21:31:11 +0300

    отредактировано сообщение при ошибке выделения памяти

--- libs/data_structures/ordered_set/ordered_set.c
+++ libs/data_structures/ordered_set/ordered_set.c
@@ -4,9 +4,8 @@
 ordered_array_set_t ordered_array_set_create(size_t capacity) {
     int *data = (int *) malloc(sizeof(int) * capacity);
     if (data == NULL && capacity != 0) {
-        fprintf(stderr, "Fail memory allocated in unordered_array_set_create.\n"
-                        "File: .../libs/data_structures/ordered_set/ordered_set.h");
-        exit(-1);
+        fprintf(stderr, "Fail memory allocated in unordered_array_set_create.\n");
+        exit(1);
     }
 
     return (ordered_array_set_t) {
@@ -21,9 +20,8 @@ void ordered_array_set_shrinkToFit(ordered_array_set_t *set) {
     if (set->size != set->capacity) {
         set->data = (int *) realloc(set->data, sizeof(int) * set->size);
         if (set->data == NULL && set->size != 0) {
-            fprintf(stderr, "Fail memory allocated in ordered_array_set_shrinkToFit\n"
-                            "File: .../libs/data_structures/ordered_set/ordered_set.h");
-            exit(-1);
+            fprintf(stderr, "Fail memory allocated in ordered_array_set_shrinkToFit\n");
+            exit(1);
         }
         set->capacity = set->size;
     }
--- libs/data_structures/unordered_set/unordered_set.c
+++ libs/data_structures/unordered_set/unordered_set.c
@@ -4,9 +4,8 @@
 unordered_array_set_t unordered_array_set_create(size_t capacity) {
     int *data = (int *) malloc(sizeof(int) * capacity);
     if (data == NULL && capacity != 0) {
-        fprintf(stderr, "Fail memory allocated in unordered_array_set_create.\n"
-                        "File: .../libs/data_structures/unordered_set/unordered_set.h");
-        exit(-1);
+        fprintf(stderr, "Fail memory allocated in unordered_array_set_create.\n");
+        exit(1);
     }
     return (unordered_array_set_t) {
             data,
@@ -20,9 +19,8 @@ void unordered_array_set_shrinkToFit(unordered_array_set_t *set) {
     if (set->size != set->capacity) {
         set->data = (int *) realloc(set->data, sizeof(int) * set->size);
         if (set->data == NULL && set->size != 0) {
-            fprintf(stderr, "Fail memory allocated in unordered_array_set_shrinkToFit\n"
-                            "File: .../libs/data_structures/unordered_set/unordered_set.h");
-            exit(-1);
+            fprintf(stderr, "Fail memory allocated in unordered_array_set_shrinkToFit\n");
+            exit(1);
         }
         set->capacity = set->size;
     }
