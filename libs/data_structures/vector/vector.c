0b401c7988fdc597b3306b54bbea9c4e73cf3fec
Pavel Chernyshev <bungostraygit@gmail.com>
2024-02-29 21:30:14 +0300

    написана функция createVector

--- libs/data_structures/vector/vector.c
+++ libs/data_structures/vector/vector.c
@@ -1,1 +1,15 @@
 #include "vector.h"
+
+vector_t createVector(size_t capacity) {
+    int *data = (int *) malloc(capacity * sizeof(int));
+    if (data == NULL && capacity != 0) {
+        fprintf(stderr, "Fail memory allocated in vectorCreate\n");
+        exit(1);
+    }
+
+    return (vector_t) {
+        data,
+        0,
+        capacity
+};
+}
