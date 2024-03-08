0a92fea16044be24af2c573f8c26e198b5ebe2a6
Pavel Chernyshev <bungostraygit@gmail.com>
2024-03-08 18:45:08 +0300

    добавлены функции использования памяти структуры vector_void

--- libs/data_structures/vector/vector_void.c
+++ libs/data_structures/vector/vector_void.c
@@ -1,5 +1,54 @@
-//
-// Created by пашк on 07.03.2024.
-//
-
 #include "vector_void.h"
+
+
+vector_void_t createVectorV(size_t capacity, size_t base_type_size) {
+    void *data = NULL;
+    if (capacity != 0) {
+        data = malloc(capacity * base_type_size);
+        if (data == NULL) {
+            fprintf(stderr, "Fail memory allocated in vectorCreateV\n");
+            exit(1);
+        }
+    }
+
+    return (vector_void_t) {
+            data,
+            0,
+            capacity,
+            base_type_size
+    };
+}
+
+
+void reserveV(vector_void_t *v, size_t new_capacity) {
+    v->capacity = new_capacity;
+
+    if (v->capacity == 0) {
+        free(v->data);
+        v->data = NULL;
+    } else {
+        v->data = realloc(v->data, v->capacity * v->base_type_size);
+        if (v->data == NULL) {
+            fprintf(stderr, "Fail memory allocated in reserveV\n");
+            exit(1);
+        }
+    }
+
+    if (v->capacity < v->size)
+        v->size = v->capacity;
+}
+
+
+void clearV(vector_void_t *v) {
+    v->size = 0;
+}
+
+
+void shrinkToFitV(vector_void_t *v) {
+    reserveV(v, v->size);
+}
+
+
+void deleteVectorV(vector_void_t *v) {
+    reserveV(v, 0);
+}
--- libs/data_structures/vector/vector_void.h
+++ libs/data_structures/vector/vector_void.h
@@ -1,4 +1,36 @@
 #ifndef COURSE_VECTOR_VOID_H
 #define COURSE_VECTOR_VOID_H
 
+#include <stdlib.h>
+#include <assert.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <memory.h>
+#include "../../algorithms/array/array.h"
+
+typedef struct vector_void {
+    void *data; // указатель на нулевой элемент вектора
+    size_t size; // размер вектора
+    size_t capacity; // вместимость вектора
+    size_t base_type_size; // размер базового типа:
+} vector_void_t;
+
+
+// возвращает вектор вместимости capacity и размером size 0
+vector_void_t createVectorV(size_t capacity, size_t base_type_size);
+
+// изменяет количество памяти, выделенное под хранение элементов вектора
+void reserveV(vector_void_t *v, size_t new_capacity);
+
+// удаляет элементы из контейнера, но не освобождает выделенную память
+void clearV(vector_void_t *v);
+
+//  освобождает память, выделенную под неиспользуемые элементы
+void shrinkToFitV(vector_void_t *v);
+
+// освобождает память, выделенную вектору и приравнивает указателю на массив data NULL,
+// размеру и вместимости size и capacity соответственно нули
+void deleteVectorV(vector_void_t *v);
+
+
 #endif //COURSE_VECTOR_VOID_H
